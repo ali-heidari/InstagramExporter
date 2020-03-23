ec5c09ed7c5853b1db27238a38aea927bd01788b
ali <ali-heidari@outlook.com>
2020-03-23 10:54:08 +0430

    Ajax request from client

--- run_server.js
+++ run_server.js
@@ -6,7 +6,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    if (req.url==="/favicon.ico") {
+    if (req.url === "/favicon.ico") {
         return;
     }
     let data = []
@@ -17,17 +17,17 @@ http.createServer(function (req, res) {
 
         res.end('Processing\n');
 
-        let file = fs.createWriteStream((new Date().getTime() / 1000).toString()+".temp.file");
+        let file = fs.createWriteStream((new Date().getTime() / 1000).toString() + ".temp.file");
 
         (async function () {
-            let res = await httpGet(decodeURIComponent(data.join('').replace('url=', '').trim()));
+            let res = await httpGet(decodeURIComponent(data.join('').trim()));
 
 
             let content = res.substr(res.indexOf('graphql') - 2);
             content = content.substr(0, content.indexOf('</script>') - 2);
-            if (content.indexOf('"is_video":false')>0) {
+            if (content.indexOf('"is_video":false') > 0) {
                 content = content.substr(content.indexOf('"display_url":') + 15);
-            } else if (content.indexOf('"is_video":true')>0) {
+            } else if (content.indexOf('"is_video":true') > 0) {
                 content = content.substr(content.indexOf('"video_url":') + 13);
             }
             content = content.substr(0, content.indexOf('",'));
