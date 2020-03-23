73eaa7dcb13d952a874a805fc339558a47e7844b
ali <ali-heidari@outlook.com>
2020-03-23 08:32:31 +0430

    init commit

---
+++ run_server.js
@@ -0,0 +1,39 @@
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const utf8 = require('utf8');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Hello World\n');
+    const file = fs.createWriteStream("pic");
+
+    (async function () {
+        let res = await httpGet("https://www.instagram.com/p/B-AKqROBEeS/?utm_source=ig_web_button_share_sheet");
+
+
+        let content = res.substr(res.indexOf('graphql') - 2);
+        content = content.substr(0, content.indexOf('</script>') - 2);
+        content = content.substr(content.indexOf('"display_url":') + 15);
+        content = content.substr(0, content.indexOf('",'));
+        content = content.replace(/\\u0026/g, "&");
+
+        https.get(content, res => {
+            res.pipe(file);
+        });
+    })();
+}).listen(3000, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:3000/');
+
+const httpGet = url => {
+    return new Promise((resolve, reject) => {
+        https.get(url, res => {
+            res.setEncoding('utf8');
+            let body = '';
+            res.on('data', chunk => body += chunk);
+            res.on('end', () => resolve(body));
+        }).on('error', reject);
+    });
+};
