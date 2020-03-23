b1dea7d5e0adf652896446bcd677b7b4454c4a2e
ali <ali-heidari@outlook.com>
2020-03-23 10:18:25 +0430

    Get the post link from client

--- run_server.js
+++ run_server.js
@@ -1,37 +1,48 @@
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
-const utf8 = require('utf8');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.end('Hello World\n');
-    const file = fs.createWriteStream("video");
+    if (req.url==="/favicon.ico") {
+        return;
+    }
+    let data = []
+    req.on('data', chunk => {
+        data.push(chunk)
+    })
+    req.on('end', () => {
 
-    (async function () {
-        // let res = await httpGet("https://www.instagram.com/p/B-AKqROBEeS/?utm_source=ig_web_button_share_sheet");
-        let res = await httpGet("https://www.instagram.com/p/B-C0QHdhkQN/?utm_source=ig_web_button_share_sheet");
+        res.end('Processing\n');
 
-        let content = res.substr(res.indexOf('graphql') - 2);
-        content = content.substr(0, content.indexOf('</script>') - 2);
-        if (content.contains('"is_video":false')) {
-            content = content.substr(content.indexOf('"display_url":') + 15);
-        } else if (content.contains('"is_video":true')) {
-            content = content.substr(content.indexOf('"video_url":') + 13);
-        }
-        content = content.substr(0, content.indexOf('",'));
-        content = content.replace(/\\u0026/g, "&");
+        let file = fs.createWriteStream((new Date().getTime() / 1000).toString()+".temp.file");
 
-        https.get(content, res => {
-            res.pipe(file);
-        });
-    })();
+        (async function () {
+            let res = await httpGet(decodeURIComponent(data.join('').replace('url=', '').trim()));
+
+
+            let content = res.substr(res.indexOf('graphql') - 2);
+            content = content.substr(0, content.indexOf('</script>') - 2);
+            if (content.indexOf('"is_video":false')>0) {
+                content = content.substr(content.indexOf('"display_url":') + 15);
+            } else if (content.indexOf('"is_video":true')>0) {
+                content = content.substr(content.indexOf('"video_url":') + 13);
+            }
+            content = content.substr(0, content.indexOf('",'));
+            content = content.replace(/\\u0026/g, "&");
+
+            https.get(content, res => {
+                res.pipe(file);
+            });
+        })();
+    })
 }).listen(3000, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3000/');
 
 const httpGet = url => {
+    console.log(url);
     return new Promise((resolve, reject) => {
         https.get(url, res => {
             res.setEncoding('utf8');
