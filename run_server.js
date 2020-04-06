6ebef0958af78efd9792e7aceb4297490b7de9e1
ali <ali-heidari@outlook.com>
2020-04-06 15:19:20 +0430

    Bug fixed

--- run_server.js
+++ run_server.js
@@ -1,6 +1,7 @@
 // Import modules
 const https = require('https');
 const http = require('http');
+const archiver = require('archiver');
 
 // Create a server and accept incoming request
 http.createServer(function (req, res) {
@@ -18,35 +19,56 @@ http.createServer(function (req, res) {
 
         (async function (response) {
             // Request to the given url, which is instagram post url
-            let res = await httpGet(postURL);
+            let result = await httpGet(postURL);
             // Instagram pages contain a JavaScript object which after load, converts to HTML element
             // This means we do not have access to HTML elements before some scripts to run 
             // Extract the script content  
-            let content = res.substr(res.indexOf('graphql') - 2);
-            content = content.substr(0, content.indexOf('</script>') - 2);
-            // Examine the video or image
-            if (content.indexOf('"is_video":false') > 0) {
-                content = content.substr(content.indexOf('"display_url":') + 15);
-            } else if (content.indexOf('"is_video":true') > 0) {
-                content = content.substr(content.indexOf('"video_url":') + 13);
-            }
-            content = content.substr(0, content.indexOf('",'));
-            // Some encoding problems need to be solved manually
-            content = content.replace(/\\u0026/g, "&");
-            // Request the file and store it in a temp file
-            https.get(content, res => {
-                // Set response headers
-                response.writeHead(200, {
-                    'Content-Type': res.headers['content-type'],
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-                });
-                // Write into client
-                res.pipe(response);
+            let content = result.substr(result.indexOf('graphql') - 2);
+            content = content.substr(0, content.indexOf('</script>'));
+            content = content.substr(0, content.indexOf("]},\"hostname\""));
+            let jsonObj = JSON.parse(content);
+            let short_media = jsonObj.graphql.shortcode_media;
+            let slides = NaN;
+            if (short_media.edge_sidecar_to_children == null) {
+                slides = [1];
+                slides[0] = new Object();
+                slides[0]["node"] = new Object();
+                if (short_media.is_video)
+                    slides[0]["node"]["video_url"] = short_media.video_url;
+                else
+                    slides[0]["node"]["display_url"] = short_media.display_url;
+                slides[0]["node"]["is_video"] = short_media.is_video;
+            } else
+                slides = short_media.edge_sidecar_to_children.edges;
+            let res = NaN;
+
+            // Set response headers
+            response.writeHead(200, {
+                'Content-Type': 'application/zip', //res.headers['content-type'],
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
             });
+
+            const zip = archiver('zip');
+            for (let slide of slides) {
+                if (slide.node.is_video) {
+                    res = await downloadFile(slide.node.video_url);
+                } else
+                    res = await downloadFile(slide.node.display_url);
+
+                zip.append(res, {
+                    name: new Date().getTime() + "." + res.headers["content-type"].split("/")[1]
+                })
+            }
+            // Send the file to the page output.
+            zip.pipe(response);
+
+            zip.finalize();
+
+
         })(res);
-    })
-}).listen(3000, "127.0.0.1");
+    });
+}).listen(3000, "0.0.0.0");
 console.log('Server running at http://127.0.0.1:3000/');
 
 /**
@@ -63,3 +85,29 @@ const httpGet = url => {
         }).on('error', reject);
     });
 };
+
+/**
+ * Download the file from instagram
+ * @param url url of file
+ */
+const downloadFile = url => {
+    return new Promise((resolve, reject) => {
+        // Request the file and store it in a temp file
+        https.get(url, res => {
+            resolve(res);
+        });
+    });
+}
+
+const streamsIntoZip = (streams, index) => {
+    return new Promise((resolve, reject) => {
+        zip.addFile(streams[index], {
+            name: new Date().getTime().toString()
+        }, function () {
+            if (streams.length < index - 1) {
+                streamsIntoZip(streams, index++)
+            }
+            resolve(true);
+        });
+    });
+}
